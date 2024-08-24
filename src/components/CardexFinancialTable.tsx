3e1eaf71583632a91c67c06922307316a8b52215
mrjs <mr.js.programmer@gmail.com>
2024-08-24 11:43:58 +0330

    fix : bug fix part five (add modal to cardex_financial)

--- src/components/CardexFinancialTable.tsx
+++ src/components/CardexFinancialTable.tsx
@@ -7,7 +7,9 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
-import { CardexFinancial } from "@/types/Cardex";
+import { CardexFinancial, CardexUser } from "@/types/Cardex";
+
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
 
 const tableName = [
   "زمان",
@@ -20,6 +22,8 @@ const tableName = [
   "کاربران",
   "توضیحات",
 ];
+
+const tableNameChild = ["تایپ", "کاربران"];
 
 const CardexFinancialTable: React.FC<{ data: CardexFinancial[] }> = ({
   data,
@@ -41,6 +45,11 @@ const CardexFinancialTable: React.FC<{ data: CardexFinancial[] }> = ({
         </TableHeader>
         <TableBody>
           {data?.map((item, index) => {
+            // console.log(item);
+            let itemUserParse;
+            if (item.Users) {
+              itemUserParse = JSON.parse(item.Users);
+            }
             return (
               <TableRow key={index}>
                 <TableCell className="text-center gradiant_to_color gradiant_to_color bg-gradient-to-r dark:from-[#BFF098] dark:to-[#6FD6FF] from-[#fb7185] to-[#64748b] px-4 py-5 font-vazirB">
@@ -65,7 +74,45 @@ const CardexFinancialTable: React.FC<{ data: CardexFinancial[] }> = ({
                   {item.BuySellParty ? item.BuySellParty : "_"}
                 </TableCell>
                 <TableCell className="text-center gradiant_to_color gradiant_to_color bg-gradient-to-r dark:from-[#BFF098] dark:to-[#6FD6FF] from-[#fb7185] to-[#64748b] px-4 py-5 font-vazirB">
-                  {String(item?.Users)}
+                  <Dialog>
+                    <DialogTrigger>
+                      <button className="border p-4 py-3 rounded-[5px] cursor-pointer outline-none">
+                        <p className="text-white text-[14px]">
+                          برای نمایش کاربران کلیک کنید
+                        </p>
+                      </button>
+                    </DialogTrigger>
+                    <DialogContent style={{ direction: "rtl" }}>
+                      <Table className="table-fixed w-full mt-6">
+                        <TableHeader>
+                          <TableRow>
+                            {tableNameChild?.map((header, index) => (
+                              <TableHead
+                                key={index}
+                                className="text-[14px] font-vazirB text-center px-4 py-2 gradiant_to_color bg-gradient-to-r dark:from-[#a1c4fd] dark:to-[#c2e9fb] from-[#4338ca] to-[#0f766e]"
+                              >
+                                {header}
+                              </TableHead>
+                            ))}
+                          </TableRow>
+                        </TableHeader>
+                        <TableBody>
+                          {itemUserParse?.map((item : CardexUser, index : number) => {
+                            return (
+                              <TableRow key={index}>
+                                <TableCell className="text-center gradiant_to_color gradiant_to_color bg-gradient-to-r dark:from-[#BFF098] dark:to-[#6FD6FF] from-[#fb7185] to-[#64748b] px-4 py-5 font-vazirB">
+                                  {item.Typ ? item.Typ : "_"}
+                                </TableCell>
+                                <TableCell className="text-center gradiant_to_color gradiant_to_color bg-gradient-to-r dark:from-[#BFF098] dark:to-[#6FD6FF] from-[#fb7185] to-[#64748b] px-4 py-5 font-vazirB">
+                                  {item.UserName ? item.UserName : "_"}
+                                </TableCell>
+                              </TableRow>
+                            );
+                          })}
+                        </TableBody>
+                      </Table>
+                    </DialogContent>
+                  </Dialog>
                 </TableCell>
                 <TableCell className="text-center gradiant_to_color gradiant_to_color bg-gradient-to-r dark:from-[#BFF098] dark:to-[#6FD6FF] from-[#fb7185] to-[#64748b] px-4 py-5 font-vazirB">
                   {item.Description ? item.Description : "_"}
